573771f8224daee234ee49ce0ead15e17f57b709
Favour Attah-Ohiemi <favourattah80@gmail.com>
2023-05-15 16:09:08 +0100

    [Update]: Added models and interfaces

---
+++ LogService/LoggerManager.cs
@@ -0,0 +1,32 @@
+﻿using HelpDeskDemo.Application.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogService
+{
+    public class LoggerManager : ILoggerManager
+    {
+        public void LogDebugging(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogError(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogInfo(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogWarning(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
