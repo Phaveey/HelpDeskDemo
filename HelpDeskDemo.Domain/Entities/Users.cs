1a95fcd1b81bfe269eae475f12380d885c213015
Favour Attah-Ohiemi <favourattah80@gmail.com>
2023-05-17 14:22:36 +0100

    [Update]: Created new branch

--- HelpDeskDemo.Domain/Entities/Users.cs
+++ HelpDeskDemo.Domain/Entities/Users.cs
@@ -8,15 +8,14 @@ namespace HelpDeskDemo.Domain.Entities
 {
     public class Users
     {
-        
        public int User_Id { get; set; }
        public int User_Type_Code { get; set; }
        public string? FullName { get; set; }
        public string? User_PhoneNumber { get; set; }
        public string? User_EmailAdd { get; set; }
        public string? Other_user_details { get; set; }
-        public Ref_User_Types Ref_User_Types { get; set; }
-        public ICollection<Problems> Problems { get; set; }
+       public Ref_User_Types Ref_User_Types { get; set; }
+       public ICollection<Problems> Problems { get; set; }
 
     }
 }
