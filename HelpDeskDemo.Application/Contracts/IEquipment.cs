accc49c303b7d1e8353017ecbf22f56447f7c24f
Favour Attah-Ohiemi <favourattah80@gmail.com>
2023-05-22 04:19:15 +0100

    [feat]:interface for equipment

--- HelpDeskDemo.Application/Contracts/IEquipment.cs
+++ HelpDeskDemo.Application/Contracts/IEquipment.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using HelpDeskDemo.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +10,10 @@ namespace HelpDeskDemo.Application.Contracts
 {
     public interface IEquipment
     {
-        
+        Task<IEnumerable<Equipment>> GetAllAsync(bool trackChanges);
+        Task<Equipment> GetEquipmentById(int Id, bool trackChanges);
+        void CreateEquipment(Equipment entity);
+        void DeleteEquipment(Equipment entity);
+
     }
 }
