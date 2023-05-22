a3458fcd71bd3d4b6fbeeb634fbb82e39a693358
Favour Attah-Ohiemi <favourattah80@gmail.com>
2023-05-22 04:48:06 +0100

    [update]:interface for ref problem status codes

--- HelpDeskDemo.Application/Contracts/IRef_Problem_Status_Codes.cs
+++ HelpDeskDemo.Application/Contracts/IRef_Problem_Status_Codes.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HelpDeskDemo.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace HelpDeskDemo.Application.Contracts
 {
     public interface IRef_Problem_Status_Codes
     {
+        Task<IEnumerable<Ref_Problem_Status_Codes>> GetAllAsync(bool trackChanges);
+        Task<Ref_Problem_Status_Codes> GetRefProblemStatusByCode(int Id, bool trackChanges);
+        void CreateRefProblemStatusCodes(Ref_Problem_Status_Codes entity);
+        void UpdateRefProblemStatusCodes(Ref_Problem_Status_Codes entity);
+        void DeleteRefProblemStatusCodes(Ref_Problem_Status_Codes entity);
     }
 }
