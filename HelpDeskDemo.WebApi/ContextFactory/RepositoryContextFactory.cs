f4a92ed57027e198b5f3a037de3635e10d85ba2a
Favour Attah-Ohiemi <favourattah80@gmail.com>
2023-05-24 15:10:27 +0100

    repository context

---
+++ HelpDeskDemo.WebApi/ContextFactory/RepositoryContextFactory.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+
+namespace HelpDeskDemo.WebApi.ContextFactory
+{
+    public class RepositoryContextFactory : IDesignTimeDbContextFactory<RepositoryContextFactory>
+    {
+        public RepositoryContextFactory CreateDbContext(string[] args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
